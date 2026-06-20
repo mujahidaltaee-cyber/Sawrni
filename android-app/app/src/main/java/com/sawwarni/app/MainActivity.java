f8ff02d67526adf5e74f9df47280e7f0f256d1c5
mujahidaltaee-cyber <mujahid.altaee@gmail.com>
2026-06-20 08:10:32 +0000

    Fix Android splash screen build

--- android-app/app/src/main/java/com/sawwarni/app/MainActivity.java
+++ android-app/app/src/main/java/com/sawwarni/app/MainActivity.java
@@ -50,6 +50,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_Sawwarni);
         super.onCreate(savedInstanceState);
         getWindow().setStatusBarColor(getColor(R.color.navy));
         getWindow().setNavigationBarColor(getColor(R.color.navy));
